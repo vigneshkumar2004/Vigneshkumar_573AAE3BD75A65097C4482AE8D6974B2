3755ce1df578f0d2372691d7296c89dddeace4c4
vigneshkumar2004 <144243140+vigneshkumar2004@users.noreply.github.com>
2023-09-06 18:55:45 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,9 @@
+def fact_rec(n):
+  if(n==8 or n==1):
+    return 1
+  else:
+    return n*fact_rec(n-1)
+
+number = int(input("Enter a number:"))
+res = fact_rec(number)
+print("The factorial of {} is {}.".format(number, res))
